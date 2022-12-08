e74fbec4426f96f1bae1a323486bc64452e0277c
Marc Peral <m.peral@sapalomera.cat>
2022-12-08 20:29:44 +0100

    documentation: added env example file

---
+++ env.example.php
@@ -0,0 +1,13 @@
+<?php
+
+// credencials de la base de dades
+$mysqlUser = "user";
+$mysqlPassword = "password";
+$mysqlHost = "hostname";
+$mysqlDB = "dbname";
+
+// domini base per fer servir en funcions internes del lloc
+$baseDomain = "http://domini.com";
+// base url es fa servir en cas de que la estructura de la url sigui diferent a l'arrel.
+// per exemple si es troba enrere un proxy invers o en un directori diferent
+$baseUrl = "/path/to/folder/";
